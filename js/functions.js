378fc9219503493db95bc57f5162ae7950563df1
mpittas <webpittas@gmail.com>
2021-06-14 19:10:16 +0300

    Jobs page done

--- js/functions.js
+++ js/functions.js
@@ -64,7 +64,7 @@ if ($(window).width() < 960) {
   new Cuttr('.sfn-news-box--medium-heading h3', { truncate: 'words', length: 6 });
   new Cuttr('.sfn-news-box--big-heading h3', { truncate: 'words', length: 6 });
   new Cuttr('.sfn-news-box--horizontal h3', { truncate: 'words', length: 8 });
-
+  new Cuttr('.sfn-news-box--sidebar-long h3', { truncate: 'words', length: 8 });
 
   new Cuttr('.sfn-featured-newsbox__title h2', { truncate: 'words', length: 10 });
   new Cuttr('.sfn-featured-newsbox__excerpt', { truncate: 'words', length: 38 });
@@ -79,6 +79,7 @@ if ($(window).width() < 960) {
   new Cuttr('.sfn-news-box--big-heading .sfn-news-box__title h3', { truncate: 'words', length: 8 });
   new Cuttr('.sfn-news-box--horizontal .sfn-news-box__title h3', { truncate: 'words', length: 12 });
   new Cuttr('.sfn-news-box--def-vertical .sfn-news-box__title h3', { truncate: 'words', length: 16 });
+  new Cuttr('.sfn-news-box--sidebar-long h3', { truncate: 'words', length: 11 });
 
   new Cuttr('.sfn-featured-newsbox__title h2', { truncate: 'words', length: 10 });
   new Cuttr('.sfn-featured-newsbox__excerpt', { truncate: 'words', length: 38 });
