679f884d2b2207a88dc6fadde8d23ce8cc7bf060
mpittas <webpittas@gmail.com>
2021-07-02 21:58:38 +0300

    Organize showSubnav.js file and update header links

--- js/showSubnav.js
+++ js/showSubnav.js
@@ -1,3 +1,6 @@
+// ------------------------------- FUNCTIONS ------------------------------- //
+
+//  Hide subnav if you hover on a link that doesn't contain '.dropdown'
 function headerNavNoDropdown() {
   var link= $('.sfn-header-nav__inner > ul > li:not(".dropdown")');
 
@@ -11,19 +14,21 @@ function headerNavNoDropdown() {
   });
 }
 
+// Open subnav if user is on top of page
+function toggleSubnavOnScroll() {
+  if($(window).scrollTop() == 0 ) {
+    $('body').addClass('open-subnav');
+  } else {
+    $('body').removeClass('open-subnav');
+  }
+}
+
+// ------------------------------- DOCUMENT READY ------------------------------- //
 jQuery(document).ready(function($){
   // headerNavNoDropdown();
 });
 
-
-// Open menu if scrolltTop is 0 (user is on top of page)
-var distance = $('body').offset().top,
-    $window = $(window);
-
-$window.scroll(function() {
-  if($window.scrollTop() == 0 ) {
-      $('body').addClass('open-subnav');
-    } else {
-      $('body').removeClass('open-subnav');
-  }
+// ------------------------------- WINDOW SCROLL ------------------------------- //\
+$(window).scroll(function() {
+  toggleSubnavOnScroll();
 });
